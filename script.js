472e08a5b2206167478a2e87d220fb8658505eb4
Abdullah Ajaz Siddiqui <abdulajaz20@gmail.com>
2021-07-22 18:21:37 +0530

    trying to add touch interaction
    using touchmove

--- script.js
+++ script.js
@@ -22,9 +22,19 @@ canvas.addEventListener('click', (e)=>{
     
     //drawCircle();
 });
-canvas.addEventListener('mousemove',(e)=>{
+canvas.addEventListener('mousemove', (e)=>{
     mouse.x = e.x;
     mouse.y = e.y;
+    for (let i = 0 ; i < 3 ; i++){
+        particleArray.push(new Particle());
+    }
+   // drawCircle();
+})
+canvas.addEventListener('touchmove', (e)=>{
+    e.preventDefault();
+    var touch = e.touches[0]
+    mouse.x = touch.x;
+    mouse.y = touch.y;
     for (let i = 0 ; i < 3 ; i++){
         particleArray.push(new Particle());
     }
@@ -73,9 +83,22 @@ function handleParticle(){
     for(let i = 0;i< particleArray.length;i++){
         particleArray[i].update();
         particleArray[i].draw();
+        
+        for (let j = i; j < particleArray.length; j++){
+            const dx = particleArray[i].x - particleArray[j].x;  
+            const dy = particleArray[i].y - particleArray[j].y;
+            const dist = Math.sqrt(dx *dx + dy *dy )
+            if (dist < 50){
+                ctx.beginPath();
+                ctx.lineWidth = particleArray[i].size /5;
+                ctx.strokeStyle = particleArray[i].color;
+                ctx.moveTo(particleArray[i].x , particleArray[i].y);
+                ctx.lineTo(particleArray[j].x , particleArray[j].y);
+                ctx.stroke();
+            }
+        }
         if (particleArray[i].size <= 0.3){
             particleArray.splice(i,1);
-            console.log(particleArray.length);
             i--;
         }
     }
@@ -87,9 +110,29 @@ function animate(){
     ctx.fillStyle = 'rgba(0,0,0 ,0.02)';
     ctx.fillRect(0,0, canvas.width,canvas.height);
     handleParticle();
-    drawCircle();
+    //drawCircle();
     hue +=0.5;
     requestAnimationFrame(animate);
 }
 animate();
-console.log(ctx);
+
+/*gui
+gui = new dat.GUI( { autoPlace: false } )
+gui.add( opt, 'total' ).name( 'Total Orbitals' ).listen();
+gui.add( opt, 'speed' ).min( -300 ).max( 300 ).step( 1 ).name( 'Speed' );
+gui.add( opt, 'scale' ).min( 0.5 ).max( 5 ).step( 0.001 ).name( 'Scale' );
+gui.add( opt, 'jitterRadius' ).min( 0 ).max( 5 ).step( 0.001 ).name( 'Radius Jitter' );
+gui.add( opt, 'jitterHue' ).min( 0 ).max( 90 ).step( 1 ).name( 'Hue Jitter' );
+gui.add( opt, 'clearAlpha' ).min( 0 ).max( 100 ).step( 1 ).name( 'Clear Alpha' );
+gui.add( opt, 'toggleOrbitals' ).name( 'Toggle Orbitals' )
+gui.add( opt, 'orbitalAlpha' ).min( 0 ).max( 100 ).step( 1 ).name( 'Orbital Alpha' );
+gui.add( opt, 'toggleLight' ).name( 'Toggle Light' );
+gui.add( opt, 'lightAlpha' ).min( 0 ).max( 100 ).step( 1 ).name( 'Light Alpha' );
+
+gui.add( opt, 'clear' ).name( 'Clear' );
+customContainer = document.getElementById( 'gui' );
+customContainer.appendChild(gui.domElement);
+  
+document.onselectstart = function(){
+  return false;
+};*/
